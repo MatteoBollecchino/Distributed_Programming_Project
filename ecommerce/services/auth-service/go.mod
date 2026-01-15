8ade6569c89fd05a3ea51447ff1603db967b743b
MatteoBollecchino <matthbolle@gmail.com>
2026-01-15 17:29:37 +0100

    Update auth-service

--- ecommerce/services/auth-service/go.mod
+++ ecommerce/services/auth-service/go.mod
@@ -22,6 +22,6 @@ require (
 	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260114163908-3f89685c29c3 // indirect
-	google.golang.org/grpc v1.78.0 // indirect
+	google.golang.org/grpc v1.78.0
 	google.golang.org/protobuf v1.36.11 // indirect
 )
--- ecommerce/services/auth-service/main.go
+++ ecommerce/services/auth-service/main.go
@@ -4,22 +4,58 @@ package main
 // to use GORM
 
 import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	pb "github.com/MatteoBollecchino/Distributed_Programming_Project/ecommerce/proto/auth"
 	"github.com/MatteoBollecchino/Distributed_Programming_Project/ecommerce/services/auth-service/internal"
+	"github.com/MatteoBollecchino/Distributed_Programming_Project/ecommerce/services/auth-service/internal/repository"
 )
 
+var port = "8081"
+
 func main() {
-	authServer := internal.NewAuthServer(nil)
-	_ = authServer
+
+	// Initialize database connection with GORM
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect database: %v", err)
+	}
+
+	// Start gRPC server
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen on port %s: %v", port, err)
+	}
+
+	// Initialize repository and create default users/admins
+	authRepo := repository.NewAuthRepository(db)
+	if err := authRepo.CreateDefaultUsersAdmins(); err != nil {
+		log.Fatalf("Internal errors while creating default users: %v", err)
+	}
+
+	// Initialize AuthServer
+	authServer := internal.NewAuthServer(authRepo)
+
+	// Register gRPC server
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthenticationServiceServer(grpcServer, authServer)
+
 	/*
-		db, _ := database.New("POSTGRES_DSN")
-
-		userRepo := repository.NewUserRepository(db)
-		loginUC := auth.NewLoginUseCase(userRepo)
-		handler := http.NewHandler(loginUC)
-
-		router := http.NewRouter(handler)
-
-		log.Println("Auth service running on :8081")
-		http.ListenAndServe(":8081", router)
+		// gRPC Health Server
+		healthServer := health.NewServer()
+		healthServer.SetServingStatus("auth.AuthenticationService", healthpb.HealthCheckResponse_SERVING)
+		healthpb.RegisterHealthServer(grpcServer, healthServer)
 	*/
+
+	log.Printf("Auth service listening on port %s", port)
+
+	// Start serving
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("gRPC user server failed: %v", err)
+	}
 }
